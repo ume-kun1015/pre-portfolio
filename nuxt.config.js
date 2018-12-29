6edce563d3d0a202e3f5edd52197811173b33b5a
romukey <baymax.blackandwhite@gmail.com>
2018-12-29 20:33:06 +0900

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,14 +36,12 @@ module.exports = {
   /*
   ** Global CSS
   */
-  css: [
-  ],
+  css: [],
 
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: [
-  ],
+  plugins: [],
 
   /*
   ** Nuxt.js modules
