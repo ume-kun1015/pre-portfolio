bbf8eacfb5c4afdb99dff1b13378e1977e6ac82f
romukey <baymax.blackandwhite@gmail.com>
2019-03-09 21:20:20 +0900

    remove doughnut-chart

--- app/components/organisms/portfolio-skills.vue
+++ app/components/organisms/portfolio-skills.vue
@@ -1,66 +1,16 @@
 <template>
-  <section class="skills-section-wrapper" data-aos="fade">
+  <div class="skills-section-wrapper">
     <div class="skills-title-wrapper">
       <div class="skills-text">
         <h2>スキル</h2>
       </div>
     </div>
-    <div class="skills-chart-wrapper">
-      <portolio-doughnut-chart :data="programmingLanguageChartData" :options="programmingLanguageChartOption" />
-      <portolio-doughnut-chart :data="awsServiceChartData" :options="awsServiceChartOption" />
-    </div>
-  </section>
+  </div>
 </template>
-
-<script>
-export default {
-  data() {
-    const language = this.$constant.LANGUAGE
-    const awsService = this.$constant.AWS_SERVICE
-
-    return {
-      programmingLanguageChartData: {
-        labels: language.map(lang => lang.NAME),
-        datasets: [
-          {
-            backgroundColor: language.map(lang => lang.COLOR),
-            data: language.map(lang => lang.RATIO),
-          },
-        ],
-      },
-      programmingLanguageChartOption: {
-        maintainAspectRatio: false,
-        title: {
-          display: true,
-          text: 'プログラミング言語',
-          fontSize: 14,
-        },
-      },
-      awsServiceChartData: {
-        labels: awsService.map(aws => aws.NAME),
-        datasets: [
-          {
-            backgroundColor: awsService.map(aws => aws.COLOR),
-            data: new Array(awsService.length).fill(awsService.length / 100),
-          },
-        ],
-      },
-      awsServiceChartOption: {
-        maintainAspectRatio: false,
-        title: {
-          display: true,
-          text: '使用したことあるAWSサービス',
-          fontSize: 14,
-        },
-      },
-    }
-  },
-}
-</script>
 
 <style lang="scss" scoped>
 .skills-section-wrapper {
-  padding: 5em 0;
+  margin: 1em 0;
 
   .skills-title-wrapper {
     max-width: $breakpoint-pc-large;
