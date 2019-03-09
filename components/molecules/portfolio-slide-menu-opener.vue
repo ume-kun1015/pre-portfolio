04278c2b3958fe35c246faf54b71351051298ac3
romukey <baymax.blackandwhite@gmail.com>
2019-03-09 18:55:17 +0900

    add portfolio-slide-menu-opener

---
+++ components/molecules/portfolio-slide-menu-opener.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="portfolio-slide-menu-opener">
+    <font-awesome-icon
+      :icon="menuIconClass"
+      class="menu-icon fa-2x"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menuIconClass: ['fas', 'bars'],
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.portfolio-slide-menu-opener {
+  cursor: pointer;
+
+  .menu-icon {
+    color: $blue;
+  }
+}
+</style>
