f571afada4bdfcd2d89f95ddf3fcd507f4084849
romukey <baymax.blackandwhite@gmail.com>
2019-03-09 20:08:21 +0900

    get sns url from context

--- components/atoms/portfolio-github-btn.vue
+++ components/atoms/portfolio-github-btn.vue
@@ -1,6 +1,6 @@
 <template>
   <a
-    :href="githubUrl"
+    :href="$portfolio.github.url"
     class="portfolio-github-btn"
   >
     <font-awesome-icon class="github-icon fa-2x" :icon="githubIconClass" />
@@ -12,7 +12,6 @@ export default {
   data() {
     return {
       githubIconClass: ['fab', 'github'],
-      githubUrl: 'https://github.com/romukey',
     }
   },
 }
