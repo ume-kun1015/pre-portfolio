1cfb37675c400a927efcbdcdfd6c8b184dacc033
romukey <baymax.blackandwhite@gmail.com>
2019-03-09 23:56:02 +0900

    add error page

---
+++ components/atoms/portfolio-error-btn.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="portfolio-error-btn">
+    <font-awesome-icon class="error-icon fa-7x" :icon="errorIconClass" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      errorIconClass: ['fas', 'exclamation-circle'],
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.portfolio-error-btn {
+  display: block;
+
+  .error-icon {
+    color: $blue;
+  }
+}
+</style>
---
+++ layouts/error.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="portfolio-error-layout">
+    <div class="portfolio-error-wrapper">
+      <portfolio-error-btn class="error-icon" />
+
+      <div class="error-text">
+        {{ errorMessage }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import PortfolioErrorBtn from '~/components/atoms/portfolio-error-btn.vue'
+
+export default {
+  components: {
+    PortfolioErrorBtn,
+  },
+  props: {
+    error: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  head() {
+    return {
+      title: this.notFoundMessage,
+      meta: [this.$metaInfo.getNoIndexNoFollow()],
+    }
+  },
+  data() {
+    return {
+      notFoundMessage: 'ページが見つかりません',
+    }
+  },
+  computed: {
+    errorMessage() {
+      return this.error.statusCode === 404 ? this.notFoundMessage : this.error.message
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.portfolio-error-layout {
+  background: $dark_navy;
+  height: calc(100vh - 100px);
+
+  .portfolio-error-wrapper {
+    padding: 100px 75px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    .error-text {
+      color: $blue;
+      margin-top: 50px;
+      font-size: 20px;
+      font-weight: bold;
+    }
+  }
+}
+</style>
