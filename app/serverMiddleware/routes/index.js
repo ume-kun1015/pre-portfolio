a405520ae8e55142dd1effb5f60930b4ba939071
romukey <baymax.blackandwhite@gmail.com>
2019-08-10 15:49:16 +0900

    cache response from qiita items

--- app/serverMiddleware/routes/index.js
+++ app/serverMiddleware/routes/index.js
@@ -1,8 +1,13 @@
 import qiita from './qiita'
+import apicache from 'apicache'
+
+const onlyStatus200 = (req, res) => res.statusCode === 200
+const cacheSuccesses = apicache.middleware('1 hour', onlyStatus200)
 
 class Routes {
   addAll(router) {
-    router.get('/qiita/items', qiita.getItems)
+    // qiitaの仕様だと1時間に60回しかリクエストが送れないみたいなので、オンメモリでキャッシュさせる。
+    router.get('/qiita/items', cacheSuccesses, qiita.getItems)
 
     return router
   }
