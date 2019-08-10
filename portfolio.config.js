aa4e2c2062971010b362aa48bc495b6830cc1874
romukey <baymax.blackandwhite@gmail.com>
2019-08-10 16:46:14 +0900

    add root url

--- portfolio.config.js
+++ portfolio.config.js
@@ -1,9 +1,22 @@
-const localDomain = 'localhost:3000'
+const prdUrl = 'https://portfolio-3bcd1.firebaseapp.com'
+const devUrl = 'https://dev-portfolio-6bfd1.firebaseapp.com/'
+const localUrl = 'http://localhost:3000'
+
+function getRootUrl() {
+  switch (process.env.portfolio_env) {
+    case 'prd':
+      return prdUrl
+    case 'dev':
+      return devUrl
+    default:
+      return localUrl
+  }
+}
 
 module.exports = {
   api: {
-    baseURL: `http://${localDomain}/api`,
-    browserBaseURL: `http://${localDomain}/api`,
+    baseURL: `${getRootUrl()}/api`,
+    browserBaseURL: `${getRootUrl()}/api`,
   },
   twitter: {
     url: 'https://twitter.com/romukey',
