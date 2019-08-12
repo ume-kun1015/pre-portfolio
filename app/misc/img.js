ae88605a7a13d2c0b72a1fc14b6f87244b0f1893
romukey <baymax.blackandwhite@gmail.com>
2019-08-12 16:04:36 +0900

    delete img.js

--- app/misc/img.js
+++
@@ -1,7 +0,0 @@
-import path from 'path'
-
-export default {
-  addPortfolioPrefix(imagePath) {
-    return process.env.DEPLOY_ENV === 'GH_PAGES' ? path.join('/portfolio', imagePath) : imagePath
-  },
-}
