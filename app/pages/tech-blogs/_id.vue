f62c660e5d58182f1de00da0d5ab865c492a6de1
romukey <baymax.blackandwhite@gmail.com>
2019-09-07 17:19:17 +0900

    add tech blog item page

---
+++ app/pages/tech-blogs/_id.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="portfolio-tech-blog">
+    {{ techBlogItem.title }}
+  </div>
+</template>
+
+<script lang="ts">
+import { mapGetters } from 'vuex'
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component({
+  computed: {
+    ...mapGetters({
+      techBlogItem: 'tech-blog-item/techBlogItem',
+    }),
+  },
+})
+export default class TechBlog extends Vue {
+  head() {
+    return {
+      title: this.techBlogItem.title,
+    }
+  }
+
+  fetch({ app, params, store, error }) {
+    return app.$axiosErrorHandler(store.dispatch('tech-blog-item/fetch', { id: params.id }), error)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.portfolio-tech-blog {
+  background: $dark_navy;
+  color: $blue;
+  height: 100vh;
+
+  .title {
+    text-align: center;
+  }
+}
+</style>
